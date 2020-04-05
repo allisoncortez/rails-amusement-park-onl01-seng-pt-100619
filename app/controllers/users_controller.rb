21219ade5a24cceb0cdb91b4917b4b6c52bd3660
allisoncortez <allizetorc@gmail.com>
2020-04-05 15:11:15 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
     def create
     
         # @user = User.find_or_create_by(name: params[:user][:name])
-        @user = User.create(user_params)
         if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
             redirect_to user_path(@user)
         else
             render :new
