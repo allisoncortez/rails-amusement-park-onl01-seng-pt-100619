eb2127756c126f732806f06e2c7757a78dc2c795
allisoncortez <allizetorc@gmail.com>
2020-04-06 13:38:07 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
 class UsersController < ApplicationController
     skip_before_action :verified_user, only: [:new, :create]
-    
+
     def new
         @user = User.new
     end
 
     def create
-    
-        # @user = User.find_or_create_by(name: params[:user][:name])
-        if (user = User.create(user_params))
-            session[:user_id] = user.id
-            redirect_to user_path(user)
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
             render 'new'
         end
     end
 
     def show
-        @user = User.find(params[:id])
+        @user = User.find_by(id: params[:id])
+        if current_user.admin
+            if current_user != @user
+                redirect_to root_path
+            end
+        end
     end
 
     private
