f4131ac172d3f63710e6e4f124554b2a412520d3
allisoncortez <allizetorc@gmail.com>
2020-04-06 13:43:52 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :happiness, :nausea, :height, :password, :password_confirmation)
+        params.require(:user).permit(:name, :happiness, :nausea, :height, :tickets, :password)
     end
 end
